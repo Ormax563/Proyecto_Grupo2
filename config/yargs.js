0e7cc2139f87566a66d38ccc64828ca64d356947
Ormax563 <j_ormaza@hotmail.com>
2019-11-16 12:56:11 -0500

    Commit 2

---
+++ config/yargs.js
@@ -0,0 +1,46 @@
+let opt = {
+    archivo: {
+        demand: true,
+        alias: 'f',
+        description: 'Ruta del archivo'
+    },
+    pais: {
+        alias: 'c',
+        default: 'ECU',
+        description: 'Indica el pais'
+    },
+    anio: {
+        alias: 'y',
+        default: 1960,
+        description: 'Indica el año'
+    }
+};
+let opt1 = {
+    archivo: {
+        demand: true,
+        alias: 'f',
+        description: 'Ruta del archivo'
+    },
+    pais: {
+        alias: 'c',
+        default: 'ECU',
+        description: 'Indica el pais'
+    },
+    anio: {
+        alias: 'y',
+        default: 1960,
+        description: 'Indica el año'
+    },
+    salida: {
+        demand= true,
+        alias: 'o',
+        description: 'Indica el archivo de salida'
+    }
+};
+const argv = require('yargs')
+    .command('mostrar', 'Mostrar', opt)
+    .command('guardar', 'Guardar', opt1).help().argv;
+
+module.exports = {
+    argv
+};
