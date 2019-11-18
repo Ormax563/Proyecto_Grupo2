b85da60a26edf75003ae7591e6d8d104b370c9a2
Dani9730 <dgonzalezc2@est.ups.edu.ec>
2019-11-17 23:29:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,12 +8,28 @@ let archivo = argv.f;
 let pais = argv.c;
 let anio = argv.y;
 let name_arch = argv.o;
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
 
 switch (comando) {
     case 'publicar':
+
         compArchivo(archivo)
-            .then((archivo) => { leerArchivo(archivo, anio, pais); })
+            .then((archivo) => {
+
+
+
+
+                leerArchivo(archivo, anio, pais);
+
+
+
+            })
             .catch((archivo) => { console.log("El archivo no existe"); })
+
         break;
     case 'guardar':
         guardarArchivo(archivo, anio, pais, name_arch)
