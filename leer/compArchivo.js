8319e8a6072dd07e5e52c39418545039807b8ad6
Dani9730 <dgonzalezc2@est.ups.edu.ec>
2019-11-17 23:27:52 -0500

    Update compArchivo

--- leer/compArchivo.js
+++ leer/compArchivo.js
@@ -100,7 +100,10 @@ let leerArchivo = (archivo, anio, pais) => {
     var suscrip = [];
     nom = [];
     cod = [];
-    data = ''
+    data = '';
+    const http = require('http');
+    const hostname = '127.0.0.1';
+    const port = 3000;
     fs.createReadStream(archivo)
         .pipe(csv())
         .on('data', (row) => {
@@ -156,6 +159,37 @@ let leerArchivo = (archivo, anio, pais) => {
                 let pos = rankin(aa, suscrip);
                 col.write(4, `${k+1}) Pais: ${nom[pos]} Suscriptores: ${suscrip[pos]}`);
             }
+            data = "Datos:  Suscripciones a telefonía celular móvil \n";
+            data = data + "Nombre:  " + `${nombrePais}` + "\n";
+            data = data + "Codigo:  " + `${pais}` + "\n";
+            data = data + "Año:    " + anio + " \n";
+            data = data + `\nMedia de suscripciones en el año ${anio} es ${mediaMundial}`;
+            data = data + `\nEl valor de las suscripciones del país ${nombrePais} con código ${pais} y valor ${valorPais} es ${estado} al promedio mundial`;
+            data = data + `\nLos cinco paises por encima del valor de suscripciones de ${nombrePais} son: \n`;
+            for (var x in mayores) {
+                numb = mayores[x];
+                data = data + "Codigo : " + cod[numb] + ", Suscripcion: " + suscrip[numb] + ", Nombre: " + nom[numb] + "\n";
+            }
+            data = data + `\nLos cinco paises por debajo del valor de suscripciones de ${nombrePais} son: \n`;
+            for (var x in menores) {
+                numb = menores[x];
+                data = data + "Codigo : " + cod[numb] + ", Suscripcion: " + suscrip[numb] + ", Nombre: " + nom[numb] + "\n";
+            }
+            data = data + '\nTop 5 del año específico \n';
+            for (var k = 0; k < 5; k++) {
+                let aa = suscrip[suscrip.length - 1 - k];
+                let pos = rankin(aa, suscrip);
+                data = data + `${k+1}) Pais: ${nom[pos]} Suscriptores: ${suscrip[pos]}` + " \n";
+            }
+            const server = http.createServer((req, res) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/plain');
+                res.end(data);
+            });
+
+            server.listen(port, hostname, () => {
+                console.log(`Server running at http://${hostname}:${port}/`);
+            });
             return data
 
         });
